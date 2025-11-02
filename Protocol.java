c18654bfc2521a8a26eabbcda2fa9886961cc9ae
harinik1510 <kharini1510@gmail.com>
2025-11-02 19:36:53 +0530

    Create Protocol.java

---
+++ Protocol.java
@@ -0,0 +1,6 @@
+public class Protocol {
+    public static final String ADD = "ADD";
+    public static final String REMOVE = "REMOVE";
+    public static final String VIEW = "VIEW";
+    public static final String EXIT = "EXIT";
+}
